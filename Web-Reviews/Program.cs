14a2b366d6523bd3df4b3d3070f2643afea0f7db
RomanEger <egerromaan@gmail.com>
2024-01-31 20:49:27 +0600

    fix

--- Web-Reviews/Program.cs
+++ Web-Reviews/Program.cs
@@ -5,9 +5,9 @@ namespace Web_Reviews
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
             builder.Services.AddMvc();
+
+            var app = builder.Build();
 
             app.MapControllerRoute(
                 name: "default",
