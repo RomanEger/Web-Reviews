aa5f169cf6e388c538de720c805765fcc8fd0226
RomanEger <egerromaan@gmail.com>
2024-02-01 21:10:27 +0600

    добавил заготовки для авторизации и перехода в профиль

--- Web-Reviews/Controllers/HomeController.cs
+++ Web-Reviews/Controllers/HomeController.cs
@@ -4,10 +4,35 @@ namespace Web_Reviews.Controllers;
 
 public class HomeController : Controller
 {
+    // private IRepository _repository;
+    //
+    // public HomeController(IRepository repository)
+    // {
+    //     _repository = repository;
+    // }
+
+
     [HttpGet]
-    [Route("")] [Route("[action]")]
-    public IActionResult Index()
-    {
-        return View();
-    }
+    [Route("")]
+    [Route("[action]")]
+    public IActionResult Index() => View();
+
+    // [HttpGet]
+    // public IActionResult Auth() => View();
+
+    // [HttpGet]
+    // public IActionResult Login(Guid id) => View(model:repository.GetUser(id));
+
+    // [HttpPost]
+    // public IActionResult Auth(string login, string password)
+    // {
+    //     var user = repository.GetUser(login);
+    //     if(user?.Password == password)
+    //         return View("Index",model:user.Id);
+    //     else
+    //     {
+    //         ViewBag.DataContentBag = "Некорректный логин или пароль. Попробуйте снова";
+    //         return View();
+    //     }
+    // }
 }
