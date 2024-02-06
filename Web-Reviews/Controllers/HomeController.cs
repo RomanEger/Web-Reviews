a748089ac7baf7a35de613f5a7683b7faa0e8c03
RomanEger <egerromaan@gmail.com>
2024-02-06 21:51:38 +0600

    init commit

--- Web-Reviews/Controllers/HomeController.cs
+++ Web-Reviews/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
 namespace Web_Reviews.Controllers;
 
 public class HomeController : Controller
@@ -10,12 +9,20 @@ public class HomeController : Controller
     // {
     //     _repository = repository;
     // }
+    private HttpClient _client = new HttpClient();
 
-
+    private readonly string _urlApi = "https://localhost:5084";
     [HttpGet]
     [Route("")]
     [Route("[action]")]
-    public IActionResult Index() => View();
+    public async Task<IActionResult> Index()
+    {
+        //var response = await _client.GetAsync(_urlApi + "/api/");
+        //мб какая-то обработка, преобразование response
+        // var obj = await response.Content.ReadAsStringAsync();
+        // var data = JsonConvert.DeserializeObject<List<model>>(obj) ?? new();
+        return View();
+    }
 
     // [HttpGet]
     // public IActionResult Auth() => View();
